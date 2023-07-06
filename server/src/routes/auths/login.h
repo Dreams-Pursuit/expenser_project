7994a9b1d340f5dab570842dcd4a69ecc7062d17
masandrei <andreymarshin@gmail.com>
2023-07-07 00:22:01 +0200

    Added routes in src

---
+++ server/src/routes/auths/login.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string.h>
+#include "../routes.h"
+
+class login: public RouteCollection{
+    public:
+        static void getLogin(crow::SimpleApp& app, sqlpp::postgresql::connection& db);
+    private:
+        static bool verifyPassword(std::string Password, std::string Original_Password);
+}
