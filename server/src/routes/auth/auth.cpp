03ae04bb6d09c08820057a28f0bac5348f3c851b
Yuriy (M) <macbook@MacBook-Pro-Macbook.local>
2023-07-11 21:51:08 +0200

    Basic register mathod was implemented

--- server/src/routes/auth/auth.cpp
+++ server/src/routes/auth/auth.cpp
@@ -41,8 +41,23 @@ void AuthRoutes::getRoutes(crow::SimpleApp& app, sqlpp::postgresql::connection& 
         catch(...){
             return crow::response(400, "probably json syntax error");
         }
-        std::cout << x["username"] << std::endl;
-        return crow::response(200,"OK");
+        if (x.has("email") && x.has("password") && x.has("main_currency")) {
+            using namespace Expenser;
+
+            AccountData acc;
+            std::string email (x["email"]);
+            std::string password (x["password"]);
+            std::string mainCurrency (x["main_currency"]);
+
+            auto ifExists = db(select(acc.EMAIL).from(acc).where(acc.EMAIL == email));
+            if (!ifExists.empty()) {
+                return crow::response(400,"The user with this email already exists");
+            }
+
+            db(insert_into(acc).set(acc.EMAIL = email, acc.PASSWORD = password, acc.mainCurrency = mainCurrency));
+            return crow::response(201,"The user was created");
+        }
+        return crow::response(400,"Bad Request");
     });
 }
 
