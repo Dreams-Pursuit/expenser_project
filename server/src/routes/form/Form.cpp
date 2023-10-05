90616b06ee144c2eae4849ee9c4c34851c5d38f1
Jyrko <uriydemoshenko@gmail.com>
2023-10-05 16:03:11 +0200

    Auth header parse in Form.cpp

--- server/src/routes/form/Form.cpp
+++ server/src/routes/form/Form.cpp
@@ -5,19 +5,21 @@
 void Form::getRoutes(crow::App<crow::CORSHandler, AuthedUser>& app, sqlpp::postgresql::connection& db) {
     CROW_ROUTE(app, "/form/generate_url").methods("POST"_method)([&db](const crow::request& req) {
         crow::json::rvalue x; //rvalue - read value
+        std::string authToken;
         try{
                 x = crow::json::load(req.body);
+                authToken = req.get_header_value("Authorization");
         }
         catch(...){
             return crow::response(400, "probably json syntax error");
         }
-        if (!x.has("access_token")) return crow::response(403, "No access token");
+        if (!authToken.size()) return crow::response(403, "No access token");
         if (x.has("username") && x.has("user_id")) {
             std::string username (x["username"]);
             std::string userId (x["user_id"]);
-            std::string access_token (x["access_token"]);
 
-            if (JWT::verifyToken(access_token, stoi(userId), CREDENTIAL_SALT::ACCESS_TOKEN_SALT) != JWT::TOKEN_VERIFICATION_STATUS::VALID) return crow::response(403, "Invalid access_token");
+
+            if (JWT::verifyToken(authToken, stoi(userId), CREDENTIAL_SALT::ACCESS_TOKEN_SALT) != JWT::TOKEN_VERIFICATION_STATUS::VALID) return crow::response(403, "Invalid access_token");
             const std::time_t issuedAt = std::time(nullptr);
             const std::time_t expiredAt = std::time(nullptr) + pow(60, 4);
 
